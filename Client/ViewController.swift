6f36a8ec4a817a21cc47c854124023071028907f
Ertem <e.biyik@vk.team>
2025-02-14 23:41:29 +0300

    Initial Commit

---
+++ Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Client
+//
+//  Created by Biyik Ertem on 14.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
